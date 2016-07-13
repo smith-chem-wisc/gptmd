2beab5cae7e1cb2832e5e6cc88db4d8c5ac16efa
U-CHM_DEPT\lwurtz <wurtzl@ripon.edu>
2016-07-13 17:24:03 -0500

    160713 Modifications include fixing start position for proteins with ambiguous amino acids.

--- python/gptmd.py
+++ python/gptmd.py
@@ -11,11 +11,10 @@ import os.path
 import optparse
 from lxml import etree as et
 import utility
-import time
 
 HTML_NS = "http://uniprot.org/uniprot"
 XSI_NS = "http://www.w3.org/2001/XMLSchema-instance"
-NAMESPACE_MAP = {None:HTML_NS, "xsi":XSI_NS}
+NAMESPACE_MAP = {None: HTML_NS, "xsi": XSI_NS}
 UP = '{'+HTML_NS+'}'
 
 MOD_MASS_TOLERANCE = 0.02
@@ -24,13 +23,12 @@ MIN_FDR_FIRST_PASS = 100
 
 usedAccessionList = []
 unusedAccessionList = []
-nonmatchingSequences = []
 badAAList = []
 
 
 def condense_xml_entry(entry):
     for element in entry:
-        if element.tag not in [UP+'protein',UP+'accession',UP+'name',UP+'gene',UP+'organism',UP+'proteinExistence',UP+'depth',UP+'sequence',UP+'feature',UP+'dbReference']:
+        if element.tag not in [UP+'protein', UP+'accession', UP+'name', UP+'gene', UP+'organism', UP+'proteinExistence', UP+'depth', UP+'sequence', UP+'feature', UP+'dbReference']:
             entry.remove(element)
         elif element.get('type') != 'modified residue' and element.tag == UP+'feature': entry.remove(element)
         elif element.get('type') != 'Ensembl' and element.tag == UP+'dbReference': entry.remove(element)
@@ -41,6 +39,7 @@ def condense_xml_entry(entry):
             for name in element:
                 if name.tag != UP+'recommendedName': element.remove(name)
         else: continue
+
 
 def enter_modification(seq_elements, prot_seq, prot_position, ptm_type):
     new_feature = et.Element(UP+'feature', type="modified residue", description=ptm_type, evidence="3")
@@ -87,9 +86,11 @@ def keep_psm(line, ptm_masses):  # So far, there is no fail-safe for blanks line
 
     return False
 
+
 def add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_masses, nterm_acetyls):
     global unusedAccessionList, usedAccessionList, nonmatchingSequences, badAAList
 
+    # Extract necessary information from the PSMs line.
     line = line.split('\t')
     protein_description, base_peptide_sequence, start_residue = line[13], line[12], int(line[14])
     precursor_mass_error = float("%.3f" % float(line[18]))
@@ -99,26 +100,24 @@ def add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_m
     if accession not in sequences:  # Ensures that the "sequences" dictionary has an entry for the "accession."
         unusedAccessionList.append(accession)
         return
-    if any((AA in set('BXZ')) for AA in sequences[accession]):  # Ensures that a bad amino acid isn't involved.
-        unusedAccessionList.append(accession)
+    protein_sequence = sequences[accession]
+
+    if any((AA in set('BXZ')) for AA in sequences[accession]):  # Ensures that a "bad" (ambiguous) amino acid isn't involved.
         badAAList.append(accession)
         protein_sequence = sequences[accession]
         possiblePeptidePositions = [i for i in range(len(protein_sequence)) if protein_sequence.startswith(base_peptide_sequence, i)]
-        if len(possiblePeptidePositions) >= 1:
-            for possPos in possiblePeptidePositions:
-                totalBadAA = sum(protein_sequence.count(AA, 0, possPos) for AA in set('BXZ'))
-                newStart = start_residue + totalBadAA
-                if possPos == newStart-1:
-                    start_residue = newStart
-        else: return
+        if len(possiblePeptidePositions) < 1:  # In case you cannot find the peptide in the protein.
+            print "Accession,", accession, ", removed because one peptide from PSMs list could not be found in this protein's sequence."
+            unusedAccessionList.append(accession)
+            return
+        if len(possiblePeptidePositions) == 1:
+            start_residue = possiblePeptidePositions[0] + 1
+        if len(possiblePeptidePositions) > 1:  # It becomes very messy to deal with multiple possible peptide possitions, so we'll remove them.
+            print "Accession,", accession, ", removed because ambiguity with the peptide position in this protein's sequence."
+            unusedAccessionList.append(accession)
+            return
 
     usedAccessionList.append(accession)
-    protein_sequence = sequences[accession]
-
-    # if start_residue != protein_sequence.find(base_peptide_sequence)+1:
-    #     print "Here's a mismatch!", accession, start_residue, protein_sequence.find(base_peptide_sequence)+1
-    #     nonmatchingSequences.append(accession)
-    #     return
 
     # Case 1 of N-terminal acetylations.
     is_n_term_acetyl_site_type1 = protein_sequence[0] == 'M' and start_residue == 1 and equals_within_tolerance(precursor_mass_error, -89.029921, MOD_MASS_TOLERANCE)
@@ -132,7 +131,7 @@ def add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_m
             position_in_peptide = base_peptide_sequence.find('K', position_in_peptide+1)
 
     # Case 2 and 3 of N-terminal acetylations
-    is_n_term_acetyl_site_type2 = protein_sequence[0] == 'M' and start_residue in [1,2] and equals_within_tolerance(precursor_mass_error, 42.01, MOD_MASS_TOLERANCE)
+    is_n_term_acetyl_site_type2 = protein_sequence[0] == 'M' and start_residue in [1, 2] and equals_within_tolerance(precursor_mass_error, 42.01, MOD_MASS_TOLERANCE)
     if is_n_term_acetyl_site_type2:
         mod_aa = base_peptide_sequence[0]   # Should be Methionine.
         if mod_aa in nterm_acetyls:
@@ -153,16 +152,15 @@ def add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_m
 
 
 def __main__():
-    global start, end
-    start = time.time()
     # Parse Command Line
     parser = optparse.OptionParser()
     # I/O
-    parser.add_option( '-x', '--reference_xml', dest='reference_xml', help='The reference UniProt-XML file.' )
-    parser.add_option( '-t', '--ptm_database', dest='ptm_database', help='Database of UniProt PTMs (slightly modified)' )
-    parser.add_option( '-s', '--psms', dest='psms', help='Peptide spectral matches tab-separated file from open search mode first-pass' )
-    parser.add_option( '-m', '--threads', dest='threads', help='Number of threads to use for adding annotations.')
-    parser.add_option( '-o', '--output', dest='output', help='The output UniProt-XML' )
+    parser.add_option( '-x', '--reference_xml', dest='reference_xml', help='The reference UniProt-XML file.  New PTM features are appended to this database to generate the output UniProt-XML protein database.')
+    parser.add_option( '-t', '--ptm_database', dest='ptm_database', help='Slightly modified database of UniProt PTMs.  This file determines which types of PTMs are included.')
+    parser.add_option( '-s', '--psms', dest='psms', help='Peptide spectral matches tab-separated.  This file is from first-pass open search and contains the mass shifts that correspond to PTMs.')
+    # parser.add_option( '-m', '--threads', dest='threads', help='Number of threads to use for adding annotations.')
+    parser.add_option( '-o', '--output', dest='output', help='Output file path.  Outputs a UniProt-XML file.')
+    # parser.add_option( '-b', '--ambiguous_residue', dest='ambiguous_residue', default=False ,  help='Manages indexing problems that occur when program is used with versions of Morpheus earlier than v261.  By default this feature will not be included.  To engage, after flag, type "True". ')
     (options, args) = parser.parse_args()
 
     # OUTPUT: new xml database
@@ -170,10 +168,10 @@ def __main__():
         outF = os.path.abspath(options.output)
         outF = open(outF, 'w')
     else:
-        print >> sys.stderr, "failed: no ouput file specified with -o or --output tag"
+        print >> sys.stderr, "Failed: no output file specified with -o or --output tag."
         exit(2)
 
-    # Parse the reference XML
+    # Parse the reference XML; Use if Iterative Parse doesn't work.
     # try:
     #     reference_xml = os.path.abspath(options.reference_xml)
     #     refXml = open(reference_xml, 'r')
@@ -187,21 +185,19 @@ def __main__():
 
 
     #Iterative Parse the reference XML
-    reference_xml = os.path.abspath(options.reference_xml)
-    refXml = open(reference_xml, 'r')
-    xml_iter = et.iterparse(refXml, remove_blank_text=True)
-
-    # try:
-    root = et.Element(UP + 'uniprot', nsmap=NAMESPACE_MAP)
-    db = et.ElementTree(root)
-    for event, el in xml_iter:
-        if el.tag == UP + "entry":
-            condense_xml_entry(el)
-            root.append(el)
-
-    # except Exception, e:
-    #     print >> sys.stderr, "Failed: error occurred while trying to parse the reference XML file. %s" %e
-    #     exit(2)
+    try:
+        reference_xml = os.path.abspath(options.reference_xml)
+        refXml = open(reference_xml, 'r')
+        xml_iter = et.iterparse(refXml, remove_blank_text=True)
+        root = et.Element(UP + 'uniprot', nsmap=NAMESPACE_MAP)
+        db = et.ElementTree(root)
+        for event, el in xml_iter:
+            if el.tag == UP + "entry":
+                condense_xml_entry(el)
+                root.append(el)
+    except Exception, e:
+        print >> sys.stderr, "Failed: error occurred while trying to parse the reference XML file. %s" %e
+        exit(2)
 
     # Parse the ptmlist
     try:
@@ -223,71 +219,61 @@ def __main__():
                 if mm != None:
                     if mm not in ptm_masses: ptm_masses[mm] = [id]
                     else: ptm_masses[mm].append(id)
-                print "just added " + id + " and " + tg
+                print "Just added " + id + " and " + tg
                 id, tg, mm = None, None, None
             elif line[0] == 'ID': id = line[1]
             elif line[0] == 'TG': tg = aa_dict[line[1].strip('.')]  # Original ptmlist ends lines with periods
             elif line[0] == 'MM': mm = float("%.3f" % float(line[1]))  # Round to 3rd place after decimal
         ptm_database.close()
     except Exception, e:
-        print >> sys.stderr, "Failed to open the UniProt PTM database. %s" % e
+        print >> sys.stderr, "Failed: cannot open the UniProt PTM database. %s" % e
         exit(2)
 
     print ptm_types
     print ptm_masses
 
     # Process the first-pass psms
-    # try:
-    psms = os.path.abspath(options.psms)
-    linect = sum(1 for line in open(psms))
-    psms = open(psms, 'r')
-    psms_list = []
+    try:
+        psms = os.path.abspath(options.psms)
+        linect = sum(1 for line in open(psms))
+        psms = open(psms, 'r')
+        psms_list = []
 
-    # Preprocess psms
-    for i, line in enumerate(psms):
-        if i % 10000 == 0: print "psms preprocessing line " + str(i) + " of " + str(linect)
-        if line.startswith('Filename'): continue
-        if keep_psm(line, ptm_masses):
-            psms_list.append(line)
-            # line = line.split('\t')
-            # protein_description = line[13]
-            # precursor_mass_error = float("%.3f" % float(line[18]))
-            # protein_description = protein_description.split('|')
-            # accession = protein_description[1]
-            # if accession == "Q9UG63" or accession == "P61981":
-            #     print i, accession, precursor_mass_error
+        # Preprocess psms
+        for i, line in enumerate(psms):
+            if i % 10000 == 0: print "Preprocessing psms line " + str(i) + " of " + str(linect)
+            if line.startswith('Filename'): continue
+            if keep_psm(line, ptm_masses):
+                psms_list.append(line)
 
-    # Add open search results
-    nterm_acetyls = utility.get_nterm_dict()
-    sequences = {}
-    sequence_elements = root.findall('.//'+UP+'sequence')
+        # Add open search results
+        nterm_acetyls = utility.get_nterm_dict()
+        sequences = {}
+        sequence_elements = root.findall('.//'+UP+'sequence')
 
-    for seq in sequence_elements:
-        accession = seq.getparent().find(UP+'accession').text
-        sequences[accession] = seq.text.replace('\n','').replace('\r','')
-    for i, line in enumerate(psms_list):
-        if i % 1000 == 0: print "psm " + str(i) + " of " + str(len(psms_list))
-        if line.startswith('Filename'): continue
-        add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_masses, nterm_acetyls)
+        for seq in sequence_elements:
+            accession = seq.getparent().find(UP+'accession').text
+            sequences[accession] = seq.text.replace('\n','').replace('\r','')
+        for i, line in enumerate(psms_list):
+            if i % 1000 == 0: print "Processing psm " + str(i) + " of " + str(len(psms_list))
+            if line.startswith('Filename'): continue
+            add_open_search_results(line, sequence_elements, sequences, ptm_types, ptm_masses, nterm_acetyls)
 
-    print "Length of unusedAccessionList:", len(unusedAccessionList)
-    print "Length of usedAccessionList:", len(usedAccessionList)
-    print "Length of nonmatchingSequences:", len(nonmatchingSequences)
-    print "nonmatchingSequences:", nonmatchingSequences
-    print "badAAList: ", badAAList
+
+        psms.close()
+    except Exception, e:
+        print >> sys.stderr, "Failed: cannot open the PSMS.tsv list. %s" % e
+        exit(2)
+
+    # print "Length of unusedAccessionList:", len(unusedAccessionList)
+    # print "Length of usedAccessionList:", len(usedAccessionList)
+    # print "badAAList: ", badAAList
     # print "unusedAccessionList:", unusedAccessionList
     # print "usedAccessionList:", usedAccessionList
-
-    psms.close()
-    # except Exception, e:
-    #     print >> sys.stderr, "Failed to open the PSMS.tsv list. %s" % e
-    #     exit(2)
 
     # Write the database
     db.write(outF, pretty_print=True)
     outF.close()
 
-    end = time.time()
-    print "Time:", end - start
 
 if __name__ == "__main__" : __main__()
